10d34bf2972b596c410645b11be8bad0eed4c034
Andres Diaz <andresf.diazm@unac.edu.co>
2021-04-21 11:11:41 -0500

    first commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "r2VBfA+6UddTdvdngVKZWSqX5sMOL2kLuT6UXIHtS3jm4dK3NbtfedeFMQIoNroiPmxGNADNqs451Axca4l4+jZRYzrXDelu1OLsKCu8W+9q1DOTGMELSk/oCMA4Ypis"
+};
---
+++ app/controllers/user.controller.js
@@ -0,0 +1,10 @@
+exports.allAccess = (req, res) => {
+  res.status(200).send("Bienvenido al Parcial Web desde la Api REST");
+};
+
+
+exports.userBoard = (req, res) => {
+  res.status(200).send("Contenido para Usuarios Registrados");
+};
+
+
